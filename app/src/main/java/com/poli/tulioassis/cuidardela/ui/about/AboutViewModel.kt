cda4447f095d80101283795e0a2eb8cb4fd2e4f8
tbassis <tulioassis.18@gmail.com>
2019-11-17 02:08:41 -0300

    Add text in fragment_about screen

--- app/src/main/java/com/poli/tulioassis/cuidardela/ui/about/AboutViewModel.kt
+++ app/src/main/java/com/poli/tulioassis/cuidardela/ui/about/AboutViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is about Fragment"
+
     }
     val text: LiveData<String> = _text
 
