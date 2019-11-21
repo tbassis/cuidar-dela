59f6d62b0da78977d2a87512053352733fa1cb70
tbassis <tulioassis.18@gmail.com>
2019-11-21 18:18:51 -0300

    Medicine and Remind Classes and activity_add_reminder.xml

--- app/src/main/java/com/poli/tulioassis/cuidardela/MainActivity.kt
+++ app/src/main/java/com/poli/tulioassis/cuidardela/MainActivity.kt
@@ -32,7 +32,6 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
                 R.id.nav_care, R.id.nav_pathology,R.id.nav_nurcing, R.id.nav_about
             ), drawerLayout
         )
---
+++ app/src/main/java/com/poli/tulioassis/cuidardela/model/Reminder.kt
@@ -0,0 +1,3 @@
+package com.poli.tulioassis.cuidardela.model
+
+class Reminder (val idReminder: Int, val nameReminder: String, val reminderImg: String)
